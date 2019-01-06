9937eeec9669313c0f76cb37260ad1bf89204c65
Nemer EL-Sahli <nemer.sahli@gmx.de>
2019-01-06 23:26:27 +0100

    branches plan with instructions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Instrunctions from './Instrunctions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Lost and found final year project</h1>
+        <Instrunctions />
       </div>
     );
   }
