32dc920d1c781b64353d7e57d6067d5221d5e17f
Nemer EL-Sahli <nemer.sahli@gmx.de>
2019-06-21 22:48:45 +0200

    organizing the files in the backend

---
+++ src/server/server2.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const appConfig = require('../config');
+const config = require('config');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+
+const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
+}
+
+// cors options
+const corsOptions = {
+  origin: appConfig.host,
+  optionsSuccessStatus: 200,
+  credentials: true
+};
+
+mongoose
+  .connect('mongodb://localhost/registick', { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+// cors middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/users', users);
+app.use('/items', items);
+app.use('/messages', messages);
+
+const port = config.PORT || 8000;
+app.listen(port, () => {
+  console.log(`server running on port ${port}...`);
+});
