1adff09d117392e2fab7447ea147ad8ed7ada100
Nemer EL-Sahli <nemer.sahli@gmx.de>
2019-06-22 23:54:35 +0200

    route for messages created

---
+++ src/server/routes/messages.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const Item = require('../schemaModel/itemModel');
+const User = require('../schemaModel/userModel');
+const auth = require('../middleware/auth');
+const router = express.Router();
+
+
+module.exports = router;
